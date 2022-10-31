49843e06e9ef5dab6fa7ede58763917b7ca4688d
LucasBrandao <lucascostabrandao@gmail.com>
2022-10-31 11:46:43 -0300

    Pokedex estilizada e responsiva. Base para consumo da API feita

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('aaaa');
+
+const url = "https://pokeapi.co/api/v2/pokemon/"
+const limit = 10
+const offset = 0
+
+fetch(url).then( response => response.json())
+  .then( responseBody => {
+    console.log(responseBody);
+  })
+  .catch( error => console.error(error))
+  .finally( () => console.log('Requisição concluída!'))
