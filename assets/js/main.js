36248dbbcc6c67bf0f9f5a76ac37e3e3950f15ac
LucasBrandao <lucascostabrandao@gmail.com>
2022-11-01 10:35:29 -0300

    Salvando...

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,33 @@
 const url = "https://pokeapi.co/api/v2/pokemon/"
 const limit = 10
 const offset = 0
+const pokemonsOl = document.getElementById('pokemonList')
 
-fetch(url).then( response => response.json())
+fetch(url)
+  .then( response => response.json())
   .then( responseBody => {
-    console.log(responseBody);
+    for (let i = 0; i < responseBody.results.length; i++) {
+      const pokemons = responseBody.results[i]
+      pokemonsOl.innerHTML += convertPokemonsHtml(pokemons)
+    }
   })
   .catch( error => console.error(error))
   .finally( () => console.log('Requisição concluída!'))
+
+  function convertPokemonsHtml(pokemons) {
+    return `
+        <li class="pokemons__card">
+          <div class="pokemons__card__name">
+            <h2>${pokemons.name}</h2>
+            <p>#001</p>
+          </div>
+          <div class="pokemons__card__details">
+            <div>
+              <p>Grass</p>
+              <p>Poisson</p>
+            </div>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemons.name}">
+          </div>
+        </li>
+      `
+  }
