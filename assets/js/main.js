83674c8ca3e0ef56e4e829e79255d9ffa0c74a62
LucasBrandao <lucascostabrandao@gmail.com>
2022-11-03 11:11:17 -0300

    Adiciona lista de pokemons dinamica e estilos por tipos de pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,31 @@
+// sass --watch assets/sass/styles.scss:assets/css/styles.css
 
-const url = "https://pokeapi.co/api/v2/pokemon/"
-const limit = 10
-const offset = 0
 const pokemonsOl = document.getElementById('pokemonList')
 
-fetch(url)
-  .then( response => response.json())
-  .then( responseBody => {
-    for (let i = 0; i < responseBody.results.length; i++) {
-      const pokemons = responseBody.results[i]
-      pokemonsOl.innerHTML += convertPokemonsHtml(pokemons)
-    }
-  })
-  .catch( error => console.error(error))
-  .finally( () => console.log('Requisição concluída!'))
+pokeApi.getPokemons().then( (pokemonsList = []) => {
+  pokemonsOl.innerHTML += pokemonsList.map(convertPokemonsLi).join('')
+})
 
-  function convertPokemonsHtml(pokemons) {
-    return `
-        <li class="pokemons__card">
-          <div class="pokemons__card__name">
-            <h2>${pokemons.name}</h2>
-            <p>#001</p>
+function convertTypeAbilits(type) {
+  return type.map(typeSlot => `<p>${typeSlot.type.name}</p>`) 
+}
+
+function convertPokemonsLi(pokemons) {
+  return `
+      <li class="pokemons__card ${pokemons.type}">
+        <div class="pokemons__card__name">
+          <h2>${pokemons.name}</h2>
+          <p>#${pokemons.id}</p>
+        </div>
+        <div class="pokemons__card__details">
+          <div>
+          ${pokemons.types.map(type => `<p>${type}</p>`).join('')}
           </div>
-          <div class="pokemons__card__details">
-            <div>
-              <p>Grass</p>
-              <p>Poisson</p>
-            </div>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemons.name}">
-          </div>
-        </li>
-      `
-  }
+          <img src="${pokemons.photo}" alt="${pokemons.name}">
+        </div>
+      </li>
+    `
+}
+
+
+
---
+++ assets/js/poke.api.js
@@ -0,0 +1,35 @@
+
+const pokeApi = {}
+
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+  return fetch(url)
+    .then( response => response.json())
+    .then( responseBody => responseBody.results)
+    .then( pokemons => pokemons.map(pokeApi.getPokemonsDetails))
+    .then( detailsRequest => Promise.all(detailsRequest))
+    .then( pokemonDetails => pokemonDetails)
+}
+
+pokeApi.getPokemonsDetails = (pokemon) => {
+  return fetch(pokemon.url)
+  .then(response => response.json())
+  .then(convertPokeApiDetailToPokemon)
+}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon()
+
+  pokemon.id = pokeDetail.order
+  pokemon.name = pokeDetail.name
+
+  const types = pokeDetail.types.map(typeSlot => typeSlot.type.name)
+  const [type]  = types
+  pokemon.types = types
+  pokemon.type = type
+
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+  return pokemon
+}
